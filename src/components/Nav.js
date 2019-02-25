c7dbb9a933c957f3a6371840ff248eac194f1c06
Stephyyy D <stephyyyd.@Stephyyys-Air.fios-router.home>
2019-02-25 08:56:50 -0500

    rounded input/remove input highlight

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,49 +6,39 @@ import FeedEditorLink from './FeedEditorLink';
 import './Nav.css';
 
 class Nav extends Component {
-  render() {
-    return (
-    <Router>
-    
-    <>
-          {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav mr-auto">
-          <img src="/images/pursuit_logo.jpg" alt="Pursuit Logo" width='42' height='42'></img>
-          <li>Pursuit Tube</li>
-            <li><Link to={'/'} className="nav-link"> HomeLink </Link></li>
-            <li><Link to={'/userlink'} className="nav-link">UserLink</Link></li>
-            <li><Link to={'/feedEditorLink'} className="nav-link">FeedEditorLink</Link></li>
-          </ul> */}
-          
-    
-        <div className="topnav">
-        <div className='link-container'>
-            <img src="/images/pursuit_logo.jpg" alt="Pursuit Logo" width='42' height='42'></img>
-            <li>Pursuit Tube</li>
-            <ul>
-                <li><Link to={'/'} className="nav-link"> HomeLink </Link></li>
-                <li><Link to={'/userlink'} className="nav-link">UserLink</Link></li>
-                <li><Link to={'/feedEditorLink'} className="nav-link">FeedEditorLink</Link></li>
-            </ul>
-            </div>
-            <div className="search-container">
-                <input type="text" placeholder="Search.." name="search"/>
-                <button type="submit">Submit</button>
-            </div>
-        </div>
-         
-         
-          {/* <hr /> */}
-    
-              <Route exact path='/' component={HomeLink} />
-              <Route path='/userlink' component={UserLink} />
-              <Route path='/feedEditorLink' component={FeedEditorLink} />
+    render() {
+        return (
+            <Router>
 
-          
-        </>
-      </Router>
-    );
-  }
+                <>
+
+
+                    <div className="topnav">
+                        <div className='link-container'>
+                            <img src="/images/pursuit_logo.jpg" alt="Pursuit Logo" width='42' height='42'></img>
+                            <li>Pursuit Tube</li>
+                            <ul>
+                                <li><Link to={'/'} className="nav-link"> HomeLink </Link></li>
+                                <li><Link to={'/userlink'} className="nav-link">UserLink</Link></li>
+                                <li><Link to={'/feedEditorLink'} className="nav-link">FeedEditorLink</Link></li>
+                            </ul>
+                        </div>
+                        <div className="search-container">
+                            <input type="text" placeholder="Search.." name="search" />
+                            <button type="submit">Submit</button>
+                        </div>
+                    </div>
+
+
+                    <Route exact path='/' component={HomeLink} />
+                    <Route path='/userlink' component={UserLink} />
+                    <Route path='/feedEditorLink' component={FeedEditorLink} />
+
+
+                </>
+            </Router>
+        );
+    }
 }
 
 export default Nav;
