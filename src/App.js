e7b12f76bbd8710ad34e2dad735dfdc213ea0e09
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-02-25 20:31:19 -0500

    adding default page with master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import Nav from './components/Nav';
+import Default from './components/Default';
 
 import './App.css';
 
 class App extends Component {
   render() {
-    return (      
-        <Nav />
+    return ( 
+      <>
+        <div>     
+          <Nav />
+        </div>
+        <div>
+          <Default />
+        </div>
+      </>
     );
   }
 }
