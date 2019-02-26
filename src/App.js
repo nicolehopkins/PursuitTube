f275d4f7d15678bc2c4ee8f5868f4447614ad422
Stephyyy D <stephyyyd.@Stephyyys-MacBook-Air.local>
2019-02-25 23:08:58 -0500

    added function to search bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Nav from './components/Nav';
 
+
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (      
+        <>
+  
         <Nav />
+      
+        </>
     );
   }
 }
