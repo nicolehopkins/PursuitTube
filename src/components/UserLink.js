57bb935c143d17d50ff9263b2c9b36491045d071
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-02-26 23:47:55 -0500

    alert add, code still has bugs

--- src/components/UserLink.js
+++ src/components/UserLink.js
@@ -1,13 +1,54 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class UserLink extends Component {
+// class UserLink extends Component {
+//   render() {
+//     return (
+//         <div>
+//           <h2>User</h2>
+//         </div>
+//     );
+//   }
+// }
+
+// export default UserLink;
+
+import React from "react";
+import { Alert } from "reactstrap";
+
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: ["default", "vicky", "eddie", "nicole", "steph"], //[]
+      newUserName: "",
+      feed:[],
+      alert: true
+    };
+  }
+  addUser = e => {
+    if (this.state.newUserName === this.state.users) { // think off diff way to check cant do string to an array - array method (.includes) or (indexOf)
+      this.setState({ alert: true });
+    } else {
+      this.setState({ alert: false });
+    }
+  };
+
+  checkListOfUsers = e => {};
+
   render() {
+
+        if(this.state.alert === true ){
+            alert ("this username is already taken, please try again")
+        }
+    
     return (
-        <div>
-          <h2>User</h2>
-        </div>
+      <>
+        <input type="text" /> 
+        <button onClick={this.addUser}>add</button>
+        {console.log('yoooooo')}
+      </>
     );
   }
 }
 
-export default UserLink;
+export default Users;
